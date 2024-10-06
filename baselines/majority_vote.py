1aecb4542d0c5403b5a0ef04d0afd415392b5935
Sushrut-naik <sushrut.r.naik@gmail.com>
2024-10-06 16:41:57 +0530

    Running inference on test using majority vote baseline

--- baselines/majority_vote.py
+++ baselines/majority_vote.py
@@ -8,39 +8,64 @@ import seaborn as sns
 
 def get_hypothesis(data: dict) -> list:
     hypothesis = {}
-    majority_ct = {} # storing the counts
+    majority_ct = {}
     for key, value in data['labels'].items():
         hypothesis[key] = value['hypothesis']
         majority_ct[key] = {'NotMentioned': 0, 'Entailment': 0, 'Contradiction': 0}
     return hypothesis, majority_ct
 
+def run_inference(test_data: dict, majority_ct: dict) -> dict:
+    predictions = {}
+    true_labels = {}
+    
+    for doc in test_data['documents']:
+        for key, value in doc['annotation_sets'][0]['annotations'].items():
+            # Get the true label for the hypothesis
+            true_labels[key] = value['choice']
+            
+            # Predict using the majority class from the training set
+            if key in majority_ct:
+                predictions[key] = majority_ct[key]
+            else:
+                predictions[key] = 'NotMentioned'
+    
+    return predictions, true_labels
+
+def evaluate(predictions: dict, true_labels: dict):
+    y_true = list(true_labels.values())
+    y_pred = list(predictions.values())
+    
+    print(classification_report(y_true, y_pred))
+    
+    cm = confusion_matrix(y_true, y_pred, labels=['NotMentioned', 'Entailment', 'Contradiction'])
+    
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=['NotMentioned', 'Entailment', 'Contradiction'], yticklabels=['NotMentioned', 'Entailment', 'Contradiction'])
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
+    plt.title('Confusion Matrix')
+    plt.show()
+
 if __name__ == "__main__":
+    # Load training data and get majority vote
     train = load_data(cfg['train_path'])
     hypothesis, majority_ct = get_hypothesis(train)
     labels = get_labels()
     
+    # Count majority votes from the training set
     for doc in train['documents']:
         for key, value in doc['annotation_sets'][0]['annotations'].items():
             majority_ct[key][value['choice']] += 1
 
+    # Find the majority vote for each hypothesis
     for key, value in majority_ct.items():
         majority_ct[key] = max(value, key=value.get)
-        
+    
+    # Load test data
     test = load_data(cfg['test_path'])
-
-    Y_true = []
-    Y_pred = []
-
-    for doc in test['documents']:
-        for key, value in doc['annotation_sets'][0]['annotations'].items():
-            Y_true.append(labels[value['choice']])
-            Y_pred.append(labels[majority_ct[key]])
-
-    report = classification_report(Y_true, Y_pred, output_dict=True)
-
-    cm = confusion_matrix(Y_true, Y_pred)
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.savefig('majority_vote.png')
+    
+    # Run inference on test data using the majority vote
+    predictions, true_labels = run_inference(test, majority_ct)
+    
+    # Evaluate predictions
+    evaluate(predictions, true_labels)
